396e6683d1993b4d44ba8de33cde3bbb14d31eea
laike9m <laike9m@gmail.com>
2013-11-15 23:31:33 +0800

    npr stations api

---
+++ NPR/stations_api.py
@@ -0,0 +1,36 @@
+import requests
+
+key = 'MDEyNTM0MTI5MDEzODM2NjY1NzgxODZlOQ001'
+
+def build_api_call(key):
+    zip_code = input("Enter your zip code:")
+    url = 'http://api.npr.org/stations?apiKey=' + key
+    url+='&format=json' 
+    url += "&zip=" + zip_code
+    return url 
+
+def call_station_api(url):
+    response = requests.get(url)
+    j = response.json()
+    return j
+    
+def parse_station_json(json_obj):
+    for station in json_obj['station']:
+        print (station['callLetters']['$text']+ ": " + 
+               station['marketCity']['$text'] + ", " + 
+               station['state']['$text'])
+        print("Frequency:" , station['frequency']['$text'] , station['band']['$text'])
+        
+        
+        if 'url' in station:
+            
+            print("MP3 Streams: ")
+            
+            for link in station['url']:
+                if link["type"] == "Audio MP3 Stream":
+                    print("\t" , link["title"], " - " , link["$text"])
+
+url = build_api_call(key)
+print("URL: ", url)
+json_obj = call_station_api(url)
+parse_station_json(json_obj)
